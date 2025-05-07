6fab0bcf34055cbcc3ef7ee3f91db33bfce96296
Yudwi <yudwiantoro@gmail.com>
2025-05-07 13:36:57 -0700

    Menambahkan Header

--- welcome.php
+++ welcome.php
@@ -2,6 +2,12 @@
   <head>
   </head>
   <body>
+    <h1>Situ Milik Ku</h1>
+	<hr>
+	  <a href="welcome.php">Welcome</a>
+	  <a href="profil.php">Profil</a>
+	  <a href="portofolio.php">Portofolio</a>
+	<hr>
     <h1>Welcome!</h1>
   </body>
 </html>
